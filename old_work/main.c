efcfb9f412cf425d5264361d2150c771a6501b02
ColinEnck <colinenck@gmail.com>
2024-10-20 12:02:11 -0400

    reorganized work

--- old_work/main.c
+++ old_work/main.c
@@ -7,14 +7,22 @@
 #define LINSIZ 80
 
 // returns length of a string
-int lenStr(char *str);
+int lenStr(const char *str);
+
+// counts the number of space chars in a string
+int countSpace(const char *str);
 
 // returns a list with the positions of all parentheses in `str`
 int *findParens(char *str);
 
 // inserts char c in str at index
 // returns new string
-char *insertStr(char *str, char c, int index);
+char *insertStr(const char *str, char c, int index);
+
+// splits a string up by space chars
+// returns an array of strings
+// assumes that the string starts and ends with a space
+char **splitAtSpace(const char *str);
 
 // parses the line of lisp code
 struct Token *parse(char *line, int len);
@@ -38,11 +46,21 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-int lenStr(char *str)
+int lenStr(const char *str)
 {
     int len;
-    for (len = 1; str[len] != '\0'; len++);
+    for (len = 0; str[len] != '\0'; len++);
     return len;
+}
+
+int countSpace(const char *str)
+{
+    int count = 0;
+    int len = lenStr(str);
+    for (int i = 0; i < len - 1; i++)
+        if (str[i] == ' ')
+            count++;
+    return count;
 }
 
 int *findParens(char *str)
@@ -67,7 +85,7 @@ int *findParens(char *str)
     return locs;
 }
 
-char *insertStr(char *str, char c, int index)
+char *insertStr(const char *str, char c, int index)
 {
     int len;
     char *newStr;
@@ -81,6 +99,42 @@ char *insertStr(char *str, char c, int index)
     strncpy(newStr + index + 1, str + index, len - index);
 
     return newStr;
+}
+
+char **splitAtSpace(const char *str)
+{
+    char **ret;
+    // position of last whitespace char
+    int last = 0;
+    // counter for ret
+    int j = 0;
+    int count = countSpace(str);
+    ret = (char **) malloc(sizeof(char *) * count - 1);
+    if (ret == NULL)
+    {
+        printf("malloc error in splitAtSpace: malloc ret\n");
+        exit(1);
+    }
+    for (int i = 0; i < count - 1; i++)
+    {
+        ret[i] = malloc(LINSIZ);
+        if (ret[i] == NULL)
+        {
+            printf("malloc error in splitAtSpace: malloc ret[i]\n");
+            exit(1);
+        }
+        bzero(ret[i], LINSIZ);
+    }
+    for (int i = 1; i < lenStr(str) - 1; i++)
+    {
+        if (str[i] == ' ') 
+        {
+            strncpy(ret[j], str + last + 1, i - last - 1);
+            j++;
+            last = i;
+        }
+    }
+    return ret;
 }
 
 struct Token *parse(char *line, int len)
@@ -123,42 +177,47 @@ struct Token *parse(char *line, int len)
 
     newLen = lenStr(newLine);
 
-    for (int i = 0; i < newLen; i++)
-        if (newLine[i] == ' ')
-            numSpace++;
+//    for (int i = 0; i < newLen; i++)
+//        if (newLine[i] == ' ')
+//            numSpace++;
 
-    locsSpace = (int *) malloc(sizeof(int) * numSpace);
-    if (locsSpace == NULL) exit(1); 
+//    locsSpace = (int *) malloc(sizeof(int) * numSpace);
+//    if (locsSpace == NULL) exit(1); 
 
-    for (int i = 0; i < newLen; i++)
-        if (newLine[i] == ')' || newLine[i] == '(')
-        {
-            locsSpace[j] = i;
-            j++;
-        }
+//    for (int i = 0; i < newLen; i++)
+//        if (newLine[i] == ')' || newLine[i] == '(')
+//        {
+//            locsSpace[j] = i;
+//            j++;
+//        }
 
-    words = (char **) malloc(sizeof(char *) * (numSpace - 1));
-    if (words == NULL) exit(1);
-    for (int i = 0; i < numSpace - 1; i++)
-    {
-        words[i] = malloc(LINSIZ);
-        if (words[i] == NULL) exit(1);
-        bzero(words[i], LINSIZ);
-    }
+//    words = (char **) malloc(sizeof(char *) * (numSpace - 1));
+//    if (words == NULL) exit(1);
+//    for (int i = 0; i < numSpace - 1; i++)
+//    {
+//        words[i] = malloc(LINSIZ);
+//        if (words[i] == NULL) exit(1);
+//        bzero(words[i], LINSIZ);
+//    }
+//
+//    printf("%s\n", newLine);
+//
+//    j = 0;
+//    last = 0;
+//    for (int i = 1; i < newLen; i++)
+//    {
+//        if (newLine[i] == ' ')
+//        {
+//            strncpy(words[j], newLine + last + 1, i - last);
+//            last = i;
+//            j++;
+//        }
+//    }
 
     printf("%s\n", newLine);
-
-    j = 0;
-    last = 0;
-    for (int i = 1; i < newLen; i++)
-    {
-        if (newLine[i] == ' ')
-        {
-            strncpy(words[j], newLine + last + 1, i - last);
-            last = i;
-            j++;
-        }
-    }
+    words = splitAtSpace(newLine);
+    for (int i = 0; i < countSpace(newLine) - 2; i++)
+        printf("\"%s\"\n", words[i]); 
 
     return tokens;
 }
